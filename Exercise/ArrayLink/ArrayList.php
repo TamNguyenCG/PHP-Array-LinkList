5281d98c482e0e5ab9505ae46816478298f0c3eb
TamNguyen <ngductam91@gmail.com>
2021-05-16 23:01:03 +0700

    Exercise Array & Link List

---
+++ Exercise/ArrayLink/ArrayList.php
@@ -0,0 +1,97 @@
+<?php
+
+
+use JetBrains\PhpStorm\Pure;
+
+class ArrayList
+{
+    public array $elements;
+    public array $originalArray;
+    public int|float $size;
+
+    public function __construct(array $array, int|float $size)
+    {
+        $this->elements = $array;
+        $this->originalArray = $array;
+        $this->size = $size;
+    }
+
+    public function size(): int
+    {
+        return count($this->elements);
+    }
+
+    #[Pure] public function isFull(): bool
+    {
+        if ($this->size < $this->size()) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public function insert($index, $obj): void
+    {
+        $this->elements[$index] = $obj;
+    }
+
+    public function add($obj): void
+    {
+        if ($this->size > $this->size()) {
+            array_push($this->elements, $obj);
+        } else {
+            echo "ArrayList is full";
+        }
+    }
+
+    public function remove($index): void
+    {
+        array_splice($this->elements, $index, 1);
+    }
+
+    public function get($index): object
+    {
+        return $this->elements[$index];
+    }
+
+    public function clear(): void
+    {
+        unset($this->elements);
+    }
+
+    public function addAll($arr): array
+    {
+        if ($this->isFull()) {
+            echo "List is full";
+        }
+        return array_merge($this->elements, $arr);
+    }
+
+    public function indexOf($obj): int
+    {
+        return $this->elements[$obj];
+    }
+
+    #[Pure] public function isEmpty(): bool
+    {
+        if ($this->size == 0 && $this->size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function sort(): array
+    {
+        sort($this->elements);
+        return $this->elements;
+    }
+
+    public function reset(): void
+    {
+        $this->elements = $this->originalArray;
+    }
+
+
+}
---
+++ Exercise/ArrayLink/index.php
@@ -0,0 +1,22 @@
+<?php
+include_once "ArrayList.php";
+
+$constArray = [1,2,3,4];
+$arraylist = new ArrayList($constArray,8);
+
+//var_dump($arraylist->addAll([5,6,7,8])) ;
+
+$arraylist->add(9);
+$arraylist->add(10);
+$arraylist->add(11);
+
+$arraylist->insert(1,100);
+$arraylist->insert(2,100);
+$arraylist->remove(2);
+$arraylist->remove(2);
+
+$arraylist->reset();
+
+echo "<pre>";
+print_r($arraylist);
+echo "</pre>";
